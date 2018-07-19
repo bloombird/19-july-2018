6531660f00327b8ece2bc2945b7203231dfc6623
Roy A Vanegas <roy@thecodeeducators.com>
2018-07-19 10:25:51 -0400

    Add basic example of referencing a DOM element

--- js/app.js
+++ js/app.js
@@ -1,16 +1,6 @@
 window.onload = function () {
     'use strict';
 
-    let number1 = 10;
-    let number2 = 20;
-    let sum = number1 + number2;
-
-    console.log(`The sum of number1 and number2 is ${sum}`);
-
-    let firstName = `Michael`;
-    let lastName = `Cheong`;
-    let fullName = `${firstName} ${lastName}`;
-
-    console.log(`The best TA ever is ${firstName} ${lastName}`);
-    console.log(`The best TA ever is ${fullName}`);
+    let body = document.getElementsByTagName(`body`)[0];
+    body.innerHTML = `<h1>Hello, world!</h1>`;
 };
