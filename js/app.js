bff7fc3caa417fea631bcc932040f3ae9d6c7c60
Roy A Vanegas <roy@thecodeeducators.com>
2018-07-19 10:13:25 -0400

    Add basic example of assignment

---
+++ js/app.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    'use strict';
+
+    let number1 = 10;
+    let number2 = 20;
+    let sum = number1 + number2;
+
+    console.log(`The sum of number1 and number2 is ${sum}`);
+
+    let firstName = `Michael`;
+    let lastName = `Cheong`;
+    let fullName = `${firstName} ${lastName}`;
+
+    console.log(`The best TA ever is ${firstName} ${lastName}`);
+    console.log(`The best TA ever is ${fullName}`);
+};
